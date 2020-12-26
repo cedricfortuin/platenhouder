921fc3721c69f923e40ab6c9db2f94bbe39e476c
cedricfortuinaccommodatiehuur <70884826+cedricfortuinaccommodatiehuur@users.noreply.github.com>
2020-12-26 11:00:14 +0100

    Update general_index.blade.php

--- resources/views/layouts/general_index.blade.php
+++ resources/views/layouts/general_index.blade.php
@@ -21,7 +21,7 @@
         integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
         crossorigin="anonymous"></script>
     <script src="{{ asset('js/sorting.js') }}"></script>
-    <title>Platenhouder</title>
+    <title>Familie Fortuin</title>
 </head>
 <body>
 <div class="container">
