da1975e3e0957c54ebfd053595b3c9ee3e0b6172
cedricfortuinaccommodatiehuur <70884826+cedricfortuinaccommodatiehuur@users.noreply.github.com>
2020-12-26 11:01:21 +0100

    Update register.blade.php

--- resources/views/admin/auth/register.blade.php
+++ resources/views/admin/auth/register.blade.php
@@ -61,6 +61,12 @@
                             </div>
                         </div>
 
+                        <div class="form-group row mb-3">
+                            <div class="col-md-6 offset-4">
+                                <a href="{{ route('login') }}">Al een account?</a>
+                            </div>
+                        </div>
+
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
