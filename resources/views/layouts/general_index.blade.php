da21458a7aaa3a98f2d1c857fd736c388d5b314b
cedricfortuinaccommodatiehuur <70884826+cedricfortuinaccommodatiehuur@users.noreply.github.com>
2020-12-30 15:45:34 +0100

    edit

--- resources/views/layouts/general_index.blade.php
+++ resources/views/layouts/general_index.blade.php
@@ -122,32 +122,6 @@
         }
     }
 
-    function pickRandomSong()
-    {
-        const number = Math.floor(Math.random() * ({{ $total }} + 1));
-
-        // Declare variables
-        let input, table, tr, td, i, txtValue;
-        input = number;
-        table = document.getElementById("recordTable");
-        tr = table.getElementsByTagName("tr");
-
-        // Loop through all table rows, and hide those who don't match the search query
-        for (i = 0; i < tr.length; i++) {
-            td = tr[i].getElementsByTagName("td")[0];
-            if (td) {
-                txtValue = td.textContent || td.innerText;
-                if (txtValue.indexOf(input) > -1) {
-                    tr[i].style.display = "";
-                    document.getElementById('randomNumber').innerText = 'Het gekozen nummer is: ' + number;
-                } else {
-                    tr[i].style.display = "none";
-                    document.getElementById('randomNumber').innerText = 'Het gekozen nummer is: ' + number;
-                }
-            }
-        }
-    }
-
     function checkIfEmpty(inputId)
     {
         let input = document.getElementById(inputId);
